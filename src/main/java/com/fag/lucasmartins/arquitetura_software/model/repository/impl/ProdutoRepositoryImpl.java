57ce4feb8231161ebe535d40c07cd15da9bf598d
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2026-03-23 16:50:25 -0300

    feat: implementando resolução do trabalho de refatoração

---
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/model/repository/impl/ProdutoRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.fag.lucasmartins.arquitetura_software.model.repository.impl;
+
+import com.fag.lucasmartins.arquitetura_software.model.bo.ProdutoBO;
+import com.fag.lucasmartins.arquitetura_software.model.repository.ProdutoRepository;
+import com.fag.lucasmartins.arquitetura_software.model.repository.entity.ProdutoEntity;
+import com.fag.lucasmartins.arquitetura_software.model.repository.jpa.ProdutoJpaRepository;
+import com.fag.lucasmartins.arquitetura_software.model.repository.mapper.ProdutoMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProdutoRepositoryImpl implements ProdutoRepository {
+
+    private final ProdutoJpaRepository produtoJpaRepository;
+
+    public ProdutoRepositoryImpl(ProdutoJpaRepository produtoJpaRepository) {
+        this.produtoJpaRepository = produtoJpaRepository;
+    }
+
+    @Override
+    public ProdutoBO salvar(ProdutoBO produtoBO) {
+        ProdutoEntity produtoEntity = ProdutoMapper.toEntity(produtoBO);
+
+        produtoJpaRepository.save(produtoEntity);
+
+        return ProdutoMapper.toBO(produtoEntity);
+    }
+}
